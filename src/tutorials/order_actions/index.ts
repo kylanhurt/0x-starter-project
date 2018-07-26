f36c5e0dac41d1e5be48328375a24a132d3680b8
Captain Crypto <kylan.hurt@gmail.com>
2018-07-26 02:00:22 -0700

    Use own personal crypto wallets for tutorial

--- src/tutorials/order_actions/index.ts
+++ src/tutorials/order_actions/index.ts
@@ -2,14 +2,14 @@ import { DecodedLogEvent, ZeroEx } from '0x.js';
 import { BigNumber } from '@0xproject/utils';
 import * as Web3 from 'web3';
 
-const TESTRPC_NETWORK_ID = 50;
+const MAINNET_NETWORK_ID = 1;
 
 // Provider pointing to local TestRPC on default port 8545
-const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
 
 // Instantiate 0x.js instance
 const configs = {
-    networkId: TESTRPC_NETWORK_ID,
+    networkId: MAINNET_NETWORK_ID,
 };
 const zeroEx = new ZeroEx(provider, configs);
 
@@ -19,56 +19,68 @@ const DECIMALS = 18;
 const mainAsync = async () => {
     // Addresses
     const WETH_ADDRESS = zeroEx.etherToken.getContractAddressIfExists() as string; // The wrapped ETH token contract
+    console.log('WETH_ADDRESS is: ', WETH_ADDRESS);
     const ZRX_ADDRESS = zeroEx.exchange.getZRXTokenAddress(); // The ZRX token contract
+    console.log('ZRX_ADDRES is: ', ZRX_ADDRESS);
     // The Exchange.sol address (0x exchange smart contract)
     const EXCHANGE_ADDRESS = zeroEx.exchange.getContractAddress();
+    console.log('EXCHANGE_ADDRESS is: ', EXCHANGE_ADDRESS);
 
     // Getting list of accounts
-    const accounts = await zeroEx.getAvailableAddressesAsync();
-    console.log('accounts: ', accounts);
+    const accounts = [
+        '0x00AC112bF28AE1D0e9569aF6844298283515F4b0', // relayer
+        '0xEd113C53b848E7e50950C9358DCAECFd6feBf747', // makerAddress = Captains Barter Test 1
+        '0xE830848074C77e0a836C999e45cc38fe451927e2', // takerAddress = Captains Barter Test 2
+    ];
+    console.log('accounts: ', JSON.stringify(accounts));
 
     // Set our addresses
-    const [makerAddress, takerAddress] = accounts;
-
+    const relayerAddress = '0x00AC112bF28AE1D0e9569aF6844298283515F4b0'.toLowerCase();
+    const makerAddress = '0xEd113C53b848E7e50950C9358DCAECFd6feBf747'.toLowerCase();
+    const takerAddress = '0xE830848074C77e0a836C999e45cc38fe451927e2'.toLowerCase();
     // Unlimited allowances to 0x proxy contract for maker and taker
     const setMakerAllowTxHash = await zeroEx.token.setUnlimitedProxyAllowanceAsync(ZRX_ADDRESS, makerAddress);
+    console.log('setMakerAllowTxHash is: ', setMakerAllowTxHash);
     await zeroEx.awaitTransactionMinedAsync(setMakerAllowTxHash);
 
     const setTakerAllowTxHash = await zeroEx.token.setUnlimitedProxyAllowanceAsync(WETH_ADDRESS, takerAddress);
+    console.log('setTakerAllowTxHash is: ', setTakerAllowTxHash);
     await zeroEx.awaitTransactionMinedAsync(setTakerAllowTxHash);
     console.log('Taker allowance mined...');
 
     // Deposit WETH
-    const ethAmount = new BigNumber(1);
-    const ethToConvert = ZeroEx.toBaseUnitAmount(ethAmount, DECIMALS); // Number of ETH to convert to WETH
+    // const ethAmount = new BigNumber(0.1);
+    // const ethToConvert = ZeroEx.toBaseUnitAmount(ethAmount, DECIMALS); // Number of ETH to convert to WETH
 
-    const convertEthTxHash = await zeroEx.etherToken.depositAsync(WETH_ADDRESS, ethToConvert, takerAddress);
-    await zeroEx.awaitTransactionMinedAsync(convertEthTxHash);
-    console.log(`${ethAmount} ETH -> WETH conversion mined...`);
+    // const convertEthTxHash = await zeroEx.etherToken.depositAsync(WETH_ADDRESS, ethToConvert, takerAddress);
+    // console.log('convertEthTxHash is: ', convertEthTxHash);
+    // await zeroEx.awaitTransactionMinedAsync(convertEthTxHash);
+    // console.log(`${ethAmount} ETH -> WETH conversion mined...`);
 
     // Generate order
     const order = {
-        maker: makerAddress,
-        taker: ZeroEx.NULL_ADDRESS,
-        feeRecipient: ZeroEx.NULL_ADDRESS,
-        makerTokenAddress: ZRX_ADDRESS,
-        takerTokenAddress: WETH_ADDRESS,
-        exchangeContractAddress: EXCHANGE_ADDRESS,
-        salt: ZeroEx.generatePseudoRandomSalt(),
-        makerFee: new BigNumber(0),
-        takerFee: new BigNumber(0),
-        makerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.2), DECIMALS), // Base 18 decimals
-        takerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.3), DECIMALS), // Base 18 decimals
-        expirationUnixTimestampSec: new BigNumber(Date.now() + 3600000), // Valid for up to an hour
+        maker: makerAddress, // Ethereum address of our Maker.
+        taker: takerAddress, // Ethereum address of our Taker.
+        feeRecipient: relayerAddress, // Ethereum address of our Relayer (none for now).
+        makerTokenAddress: ZRX_ADDRESS, // The token address the Maker is offering.
+        takerTokenAddress: WETH_ADDRESS, // The token address the Maker is requesting from the Taker.
+        exchangeContractAddress: EXCHANGE_ADDRESS, // The exchange.sol address.
+        salt: ZeroEx.generatePseudoRandomSalt(), // Random number to make the order (and therefore its hash) unique.
+        makerFee: new BigNumber(0), // How many ZRX the Maker will pay as a fee to the Relayer.
+        takerFee: new BigNumber(0), // How many ZRX the Taker will pay as a fee to the Relayer.
+        makerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.2), DECIMALS), // Base 18 decimals, The amount of ZRX token the Maker is offering.
+        takerTokenAmount: ZeroEx.toBaseUnitAmount(new BigNumber(0.3), DECIMALS), // Base 18 decimals, The amount of ZRX token the Maker is requesting from the Taker.
+        expirationUnixTimestampSec: new BigNumber(Date.now() + 3600000), // When will the order expire (in unix time), Valid for up to an hour
     };
 
     // Create orderHash
     const orderHash = ZeroEx.getOrderHashHex(order);
+    console.log('orderHash is: ', orderHash);
 
     // Signing orderHash -> ecSignature
     const shouldAddPersonalMessagePrefix = false;
     const ecSignature = await zeroEx.signOrderHashAsync(orderHash, makerAddress, shouldAddPersonalMessagePrefix);
-
+    console.log('ecSignature is: ', ecSignature);
     // Appending signature to order
     const signedOrder = {
         ...order,
@@ -80,6 +92,7 @@ const mainAsync = async () => {
 
     // Try to fill order
     const shouldThrowOnInsufficientBalanceOrAllowance = true;
+    // the amount of tokens (in our case WETH) the Taker wants to fill.
     const fillTakerTokenAmount = ZeroEx.toBaseUnitAmount(new BigNumber(0.1), DECIMALS);
 
     // Filling order
